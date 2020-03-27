65ccfa9215b07fa2ee8415ab0e405012ea7cbc7c
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2020-03-27 18:44:18 +0300

    Added frontend but not mounted vue

---
+++ covid19-tr/src/components/About.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <div class="settings-general">
+      <div class="settings-general-sm">
+
+        <p class="settings-title">Ayarlar</p>
+
+        <div class="settings-share-div">
+          <a class="share-a" href="#">Arkadaşlarınıza bizi tavsiye edin</a>
+          <img src="../assets/icon_/Shape.svg">
+        </div>
+
+        <div class="settings-social-media">
+          <p class="settings-social-media-title">TAKİP EDİN</p>
+          <p class="settings-social-media-title-thin">Ayvalık Yerel İnisiyatif</p>
+          <div class="settings-social-media-icon">
+            <img src="../assets/icon_/social.svg">
+            <img src="../assets/icon_/Shape-2.svg">
+            <img src="../assets/icon_/insta.svg">
+            <img src="../assets/icon_/ws.svg">
+          </div>
+        </div>
+
+        <div class="settings-social-media social-media-down">
+          <p class="settings-social-media-title-thin">Ayvalık Yerel İnisiyatif</p>
+          <div class="settings-social-media-icon">
+            <img src="../assets/icon_/social.svg">
+            <img src="../assets/icon_/ws.svg">
+          </div>
+        </div>
+
+        <div class="settings-footer">
+          <div class="settings-footer-left">
+            <p>from</p>
+            <img src="../assets/icon_/left-footer.svg">
+          </div>
+
+          <div class="settings-footer-right">
+            <p>from</p>
+            <img src="../assets/icon_/right-footer.svg">
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "About"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- covid19-tr/src/components/Home.vue
+++ covid19-tr/src/components/Home.vue
@@ -1,28 +1,203 @@
 <template>
   <div id="app">
-    <div class="container">
-      <br><br>
-      <h1>Covid Acil!</h1>
-      <hr>
-      <label>İli Seç</label>
-      <v-select :options="optionsProvince" v-model="province" placeholder="Zorunlu"></v-select>
-      <br>
-      <label>İlçeyi Seç</label>
-      <v-select :options="optionsDistricts" v-model="districts" placeholder="Zorunlu Değil"></v-select>
-      <br>
-      <label>Mahalleyi Seç</label>
-      <v-select :options="optionsNeighborhoods" v-model="neighborhoods" placeholder="Zorunlu Değil"></v-select>
-      <br><br>
-      <div v-for="data in datasPhone">
-        <div class="card">
-          <a :href="`tel:+${ data.no }`">
-            <p>{{ data.name }}</p>
-            <p>{{ data.no }}</p>
-            <p>{{ data.category.name }}</p>
-            <p>{{ data.province.name }}</p>
-          </a>
+    <!--    <div class="container">
+          <br><br>
+          <h1>Covid Acil!</h1>
+          <hr>
+          <label>İli Seç</label>
+          <v-select :options="optionsProvince" v-model="province" placeholder="Zorunlu"></v-select>
+          <br>
+          <label>İlçeyi Seç</label>
+          <v-select :options="optionsDistricts" v-model="districts" placeholder="Zorunlu Değil"></v-select>
+          <br>
+          <label>Mahalleyi Seç</label>
+          <v-select :options="optionsNeighborhoods" v-model="neighborhoods" placeholder="Zorunlu Değil"></v-select>
+          <br><br>
+          <div v-for="data in datasPhone">
+            <div class="card">
+              <a :href="`tel:+${ data.no }`">
+                <p>{{ data.name }}</p>
+                <p>{{ data.no }}</p>
+                <p>{{ data.category.name }}</p>
+                <p>{{ data.province.name }}</p>
+              </a>
+            </div>
+          </div>
+        </div>-->
+
+    <div class="home-general">
+      <div class=home-card>
+        <section class="container-1">
+          <div class="dropdown">
+            <select name="one" class="dropdown-select">
+              <option value="" v-for="option in optionsProvince">{{ option }}</option>
+            </select>
+          </div>
+          <h1>{{province}}</h1>
+
+          <div id="district" class="dropdown">
+            <select name="two" class="dropdown-select">
+              <option value="" v-for="option in optionsDistricts" v-model="districts">{{ option }}</option>
+            </select>
+          </div>
+        </section>
+        <div class="container">
+          <div class="card">
+            <div class="icon">
+              <img src="../assets/icon_/tum_ihtiyaclar.svg">
+            </div>
+            <div class="text-number">444 44 44</div>
+            <div class="text-title">Tüm İhtiyaçlar</div>
+            <div class="text-thin">Büyükşehir Belediye</div>
+            <div class="text-thin">Genel Çağrı Merkezi</div>
+            <div class="check">
+              <img src="../assets/icon_/star.svg">
+            </div>
+          </div>
+          <div class="card">
+            <div class="icon">
+              <img src="../assets/icon_/manav.svg">
+            </div>
+            <div class="text-number">444 44 44</div>
+            <div class="text-title">Manav</div>
+            <div class="text-thin">Çamlık</div>
+            <div class="text-thin">SMO</div>
+            <div class="check">
+              <img src="../assets/icon_/star.svg">
+            </div>
+          </div>
+          <div class="card">
+            <div class="icon">
+              <img src="../assets/icon_/faturalar.svg">
+            </div>
+            <div class="text-number">444 44 44</div>
+            <div class="text-title">Maaş ve Faturalar</div>
+            <div class="text-thin">Tüm Mahalleler</div>
+            <div class="text-thin">Emniyet</div>
+            <div class="check">
+              <img src="../assets/icon_/star.svg">
+            </div>
+          </div>
+          <div class="card">
+            <div class="icon">
+              <img src="../assets/icon_/alisveris.svg">
+            </div>
+            <div class="text-number">444 44 44</div>
+            <div class="text-title">Alışveris</div>
+            <div class="text-thin">Tüm Mahalleler</div>
+            <div class="text-thin">Belediye</div>
+            <div class="check">
+              <img src="../assets/icon_/star.svg">
+            </div>
+          </div>
+          <div class="card">
+            <div class="icon">
+              <img src="../assets/icon_/Koordinasyon.svg">
+            </div>
+            <div class="text-number">444 44 44</div>
+            <div class="text-title">Koordinasyon</div>
+            <div class="text-thin">Tüm Mahalleler</div>
+            <div class="text-thin">Yerel İnisiyatif</div>
+            <div class="check">
+              <img src="../assets/icon_/star.svg">
+            </div>
+          </div>
+
+          <div class="card">
+
+            <div class="icon">
+              <img src="../assets/icon_/psikolojikdestek.svg">
+            </div>
+            <div class="text-number">444 44 44</div>
+            <div class="text-title">Psikolojik Destek</div>
+            <div class="text-thin">Tüm Mahalleler</div>
+            <div class="text-thin">İlçe Sağlık Müdürlüğü</div>
+            <div class="check">
+              <img src="../assets/icon_/star.svg">
+            </div>
+          </div>
+
+
         </div>
+
+
       </div>
+
+
+    </div>
+
+    <div class="section-down">
+      <div class="number-section">
+        <p class="number-section-text">Numaralar</p>
+
+        <div class="section-down-card">
+          <div class="card-info">
+            <p class="info-p">BELEDİYE 1</p>
+            <p class="info2-p">Tüm Mahalleler</p>
+          </div>
+          <div class="card-number">
+            <p class="number-left">0505 806 77 14</p>
+          </div>
+        </div>
+
+        <div class="section-down-card">
+          <div class="card-info">
+            <p class="info-p">BELEDİYE 2</p>
+            <p class="info2-p">Tüm Mahalleler</p>
+          </div>
+          <div class="card-number">
+            <p class="number-left">0505 806 77 14</p>
+          </div>
+        </div>
+
+        <div class="section-down-card">
+          <div class="card-info">
+            <p class="info-p">BELEDİYE 3</p>
+            <p class="info2-p">Tüm Mahalleler</p>
+          </div>
+          <div class="card-number">
+            <p class="number-left">0505 806 77 14</p>
+          </div>
+        </div>
+
+      </div>
+
+
+      <div class="country-section">
+        <p class="country-section-text">Ülke Geneli</p>
+
+        <div class="section-down-card">
+          <div class="card-info">
+            <p class="info-p">Corona Danışma Hattı</p>
+            <p class="info2-p">Tüm Türkiye</p>
+          </div>
+          <div class="card-number">
+            <p class="number-left">182</p>
+          </div>
+        </div>
+
+        <div class="section-down-card">
+          <div class="card-info">
+            <p class="info-p">Acil</p>
+            <p class="info2-p">Tüm Türkiye</p>
+          </div>
+          <div class="card-number">
+            <p class="number-left">112</p>
+          </div>
+        </div>
+
+        <div class="section-down-card">
+          <div class="card-info">
+            <p class="info-p">Emniyet GM</p>
+            <p class="info2-p">Tüm Türkiye</p>
+          </div>
+          <div class="card-number">
+            <p class="number-left">155</p>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   </div>
 </template>
@@ -55,7 +230,6 @@
         for (let i = 0; i <= this.datasProvince.length - 1; i++) {
           if (this.datasProvince[i].name == this.province) {
             this.id = i + 1;
-            console.log("********PROVİNCE********", this.id);
             this.getDistricts();
             this.getNumber();
           }
@@ -65,7 +239,6 @@
         for (let i = 0; i <= this.datasDistricts.length - 1; i++) {
           if (this.datasDistricts[i].name == this.districts) {
             this.id = this.datasDistricts[i].id;
-            console.log("********PROVİNCE********", this.id);
             this.getNeighborhoods();
             this.getNumber();
           }
@@ -134,14 +307,5 @@
 </script>
 
 <style scoped>
-  p {
-    color: black;
-  }
 
-  .card {
-    width: 160px;
-    height: 160px;
-    background-color: seagreen;
-    border-radius: 10px;
-  }
 </style>
